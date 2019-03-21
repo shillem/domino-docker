7e67516659e87ec88c66aa22333b35fbf673154e
Shillem Volpato <volpato.sh@gmail.com>
2019-03-21 10:22:20 +0100

    Fix promptly stopping the container while running server setup

--- util/docker-entrypoint.sh
+++ util/docker-entrypoint.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-NUI_NOTESDIR_SERVER=$NUI_NOTESDIR/bin/server
 
 stop_server() {
-    $NUI_NOTESDIR_SERVER -q
+    $NUI_NOTESDIR/bin/server -q
+}
+
+stop_server_setup() {
+    kill $(ps -ef | pgrep -f 'lotus.domino.setup.Wizard')
 }
 
 if [ -f "$NUI_NOTESDIR_DATA/server.id" ]; then
     trap "stop_server" HUP INT QUIT TERM
 
-    screen -dmS console "$NUI_NOTESDIR_SERVER"
+    screen -dmS console $NUI_NOTESDIR/bin/server
 
     echo "Run 'docker container exec -it <container> screen -r console' to access the server console."
     echo "Run 'docker container stop <container>' to stop the server."
-
-    while [[ ! -z $(ps -ef | pgrep -f 'SCREEN \-dmS console') ]]; do
-        sleep 1
-    done
 else
-    $NUI_NOTESDIR_SERVER -listen 1352
+    trap "stop_server_setup" HUP INT QUIT TERM
+
+    screen -dmS console $NUI_NOTESDIR/bin/server -listen 1352
+
+    echo "Run 'docker container exec -it <container> screen -r console' to access the server setup information."
+    echo "Run 'docker container stop <container>' to stop the server setup."
 fi
+
+while [[ ! -z $(ps -ef | pgrep -f 'SCREEN \-dmS console') ]]; do
+    sleep 1
+done
