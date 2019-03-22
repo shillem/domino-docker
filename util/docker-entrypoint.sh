565f9225e23b848f87ce9881bd6230e35b763504
Shillem Volpato <volpato.sh@gmail.com>
2019-03-22 15:20:52 +0100

    Improved way commands can be sent to the  server

--- util/docker-entrypoint.sh
+++ util/docker-entrypoint.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 stop_server() {
-    $NUI_NOTESDIR/bin/server -q
+    server -q
 }
 
 stop_server_setup() {
     kill $(ps -ef | pgrep -f 'lotus.domino.setup.Wizard')
 }
 
-if [ -f "$NUI_NOTESDIR_DATA/server.id" ]; then
+if [ -f "server.id" ]; then
     trap "stop_server" HUP INT QUIT TERM
 
-    screen -dmS console $NUI_NOTESDIR/bin/server
+    screen -dmS console server
 
     echo "Run 'docker container exec -it <container> screen -r console' to access the server console."
     echo "Run 'docker container stop <container>' to stop the server."
 else
     trap "stop_server_setup" HUP INT QUIT TERM
 
-    screen -dmS console $NUI_NOTESDIR/bin/server -listen 1352
+    screen -dmS console server -listen 1352
 
     echo "Run 'docker container exec -it <container> screen -r console' to access the server setup information."
     echo "Run 'docker container stop <container>' to stop the server setup."
